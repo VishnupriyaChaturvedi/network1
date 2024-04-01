be2af61bb271aeda953fa8fa26060f2d683cf7ac
Your Name <vishnupriyachaturvedi2306@gmail.com>
2024-04-01 12:30:33 +0530

    hint.html

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-console.log("hello janascript")
+console.log("hello javascript")
 console.log(1+2)
 
 //const, var, let
@@ -6,15 +6,15 @@ console.log(1+2)
 const p1 =3.1415;
 
 var x =1;
-console.log(x)
+console.log(x);
 var y = 1.4;
-console.log(y)
+console.log(y);
 var z = "hello";
 console.log(z);
 var a =[x, y, z];
-console.log(a)
-var 1st = [1, "0", [1, 2],4.8, true];
-console.log(1st);
+console.log(a);
+var lst = [1, "0", [1, 2],4.8, true];
+console.log(lst);
 var obj = {
     name: "mr. x",
     job: "hacker",
@@ -37,7 +37,7 @@ let phone = {
 let str1 = 'hello';
 let str2 = 'world';
 let str3 = 'hello world';
-console.log(str3)
+console.log(str3);
 
 let outcome = [[1, 3, 2, 4, 5, 6],
                [4, 2, 1, 5, 3, 6],
@@ -46,4 +46,20 @@ let outcome = [[1, 3, 2, 4, 5, 6],
                [6, 5, 3, 4, 2, 1],
                [4, 5, 6, 1, 2, 3],
 ]
-console.log(outcome[1][3])
+console.log(outcome[1][3])//5
+console.log(outcome[4][3])//8
+
+let D3 = [
+    [
+        [1, 2],
+        [1, 2]
+    ],
+    [
+        [1, 2],
+        [1, 2]
+    ],
+    [
+        [1, 2],
+        [1, 2]
+    ],
+]
